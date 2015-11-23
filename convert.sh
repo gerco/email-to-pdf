60663eddbbac925c10ecc115e7be8c8da8cfd793
Gerco Dries <gerco@gdries.nl>
2015-11-23 17:59:24 -0500

    Initial commit

---
+++ convert.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -t 0 ]; then 
+	>&2 echo This script must be run with a file or pipe as stdin
+fi
+
+# First save stdin (eml format) to a file
+#cat > input.eml
+
+# Now convert that file to html
+mhonarc -single -rcfile /mhonarc.rc /vol/$1 > /tmp/input.html
+
+# And finally convert to PDF
+wkhtmltopdf /tmp/input.html /tmp/output.pdf >/dev/null
+
+# Now echo the file to stdout
+cat /tmp/output.pdf

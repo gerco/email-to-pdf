953cda3f90f7aea0fa22b6da55b5bd1ad211a2cf
Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>
2019-10-05 09:45:29 -0500

    Disable images
    I keep getting ECONNREFUSED errors when converting certain emails. Disable images to fix this with an environment variable.

--- convert.sh
+++ convert.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+DISABLE_IMAGES="${DISABLE_IMAGES:-false}"
+
 if [ -t 0 ]; then 
 	>&2 echo This script must be run with a file or pipe as stdin
 fi
@@ -11,8 +13,13 @@ fi
 # Now convert that file to html
 mhonarc -single -rcfile /mhonarc.rc "/vol/$1" > /tmp/input.html
 
-# And finally convert to PDF
-wkhtmltopdf /tmp/input.html /tmp/output.pdf >/dev/null
+# And finally convert to PDF (disable images to reduce probability of ECONNREFUSED errors)
+if [ "$DISABLE_IMAGES" == "true" ]
+then
+  wkhtmltopdf --no-images /tmp/input.html /tmp/output.pdf >/dev/null
+else
+  wkhtmltopdf /tmp/input.html /tmp/output.pdf >/dev/null
+fi
 
 # Now echo the file to stdout
 cat /tmp/output.pdf
